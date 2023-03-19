01da67f3476f48eb7c5ec4a21a99eeb538170ca8
Aditya Mukherjee <60893631+adityamukherjee42@users.noreply.github.com>
2023-03-19 13:33:37 +0530

    Adding length,popup

--- website/dist.py
+++ website/dist.py
@@ -5,6 +5,8 @@ from pymongo import MongoClient
 import osmnx as ox
 import networkx as nx
 import folium
+import base64
+from folium import IFrame
 ox.config(log_console=True, use_cache=True)
 try:
     conn = pymongo.MongoClient('mongodb+srv://aditya:12345@cluster0.rutst.mongodb.net/test')
@@ -55,15 +57,74 @@ def distance():
                                           orig_node,
                                           dest_node,
                                           weight=optimizer)
-        print(shortest_route)
-        shortest_route_map = ox.plot_route_folium(graph, shortest_route, tiles='openstreetmap')
-        start_latlng = (start_latlng[0], start_latlng[1])
-        end_latlng = (end_latlng[0], end_latlng[1])
-        start_marker = folium.Marker(location=start_latlng,icon=folium.Icon(color='green'))
-        end_marker = folium.Marker(location=end_latlng,icon=folium.Icon(color='red'))
+        le=nx.shortest_path_length(graph,orig_node,dest_node, method='dijkstra',weight=optimizer)
+        if optimizer=='time':
+            Info='It will take you '+str(le)+' minutes'
+        elif optimizer=='length':
+            Info='The distance between your source and target '+str(le)+' meters'
+        else:
+            Info='Please select your source and target'
+
+        shortest_route_map = ox.plot_route_folium(graph, shortest_route, tiles='openstreetmap',tooptip='This tooltip will appear')
+        # Add Marker
+        if source == 'Tech Park':
+            encoded = base64.b64encode(open('website/techpark.jpg', 'rb').read())
+            html = '<img src="data:image/png;base64,{}">'.format
+            iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
+            popup = folium.Popup(iframe, max_width=400)
+            start_latlng = (start_latlng[0], start_latlng[1])
+            start_marker = folium.Marker(location=start_latlng, icon=folium.Icon(color='green'), popup=popup,
+                                         tooltip=source)
+        elif source == 'BIO-Tech Block':
+            encoded = base64.b64encode(open('website/BioTech.jpg', 'rb').read())
+            html = '<img src="data:image/png;base64,{}">'.format
+            iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
+            popup = folium.Popup(iframe, max_width=400)
+            start_latlng = (start_latlng[0], start_latlng[1])
+            start_marker = folium.Marker(location=start_latlng, icon=folium.Icon(color='green'), popup=popup,
+                                         tooltip=html)
+        elif source == 'SRM University Building':
+            encoded = base64.b64encode(open('website/ub.jpg', 'rb').read())
+            html = '<img src="data:image/png;base64,{}">'.format
+            iframe = IFrame(html(encoded.decode('UTF-8')), width=400, height=350)
+            popup = folium.Popup(iframe, max_width=400)
+            start_latlng = (start_latlng[0], start_latlng[1])
+            start_marker = folium.Marker(location=start_latlng, icon=folium.Icon(color='green'), popup=popup,
+                                         tooltip=html)
+        else:
+            start_latlng = (start_latlng[0], start_latlng[1])
+            start_marker = folium.Marker(location=start_latlng, icon=folium.Icon(color='green'), popup=source,
+                                         tooltip=source)
+        if target == 'Tech Park':
+            encoded_t = base64.b64encode(open('website/techpark.jpg', 'rb').read())
+            html_t = '<img src="data:image/png;base64,{}">'.format
+            iframe_t = IFrame(html_t(encoded_t.decode('UTF-8')), width=400, height=350)
+            popup_t = folium.Popup(iframe_t, max_width=400)
+            end_latlng = (end_latlng[0], end_latlng[1])
+            end_marker = folium.Marker(location=end_latlng, icon=folium.Icon(color='red'), popup=popup_t,tooltip=html_t)
+        elif target == 'BIO-Tech Block':
+            encoded_t = base64.b64encode(open('website/BioTech.jpg', 'rb').read())
+            html_t = '<img src="data:image/png;base64,{}">'.format
+            iframe_t = IFrame(html_t(encoded_t.decode('UTF-8')), width=400, height=350)
+            popup_t = folium.Popup(iframe_t, max_width=400)
+            end_latlng = (end_latlng[0], end_latlng[1])
+            end_marker = folium.Marker(location=end_latlng, icon=folium.Icon(color='red'), popup=popup_t,
+                                       tooltip=html_t)
+        elif target == 'SRM University Building':
+            encoded_t = base64.b64encode(open('website/ub.jpg', 'rb').read())
+            html_t = '<img src="data:image/png;base64,{}">'.format
+            iframe_t = IFrame(html_t(encoded_t.decode('UTF-8')), width=400, height=350)
+            popup_t = folium.Popup(iframe_t, max_width=400)
+            end_latlng = (end_latlng[0], end_latlng[1])
+            end_marker = folium.Marker(location=end_latlng, icon=folium.Icon(color='red'), popup=popup_t,
+                                       tooltip=html_t)
+        else:
+            end_latlng = (end_latlng[0], end_latlng[1])
+            end_marker = folium.Marker(location=end_latlng, icon=folium.Icon(color='red'), popup=target,
+                                       tooltip=target)
         # add the circle marker to the map
         start_marker.add_to(shortest_route_map)
         end_marker.add_to(shortest_route_map)
         shortest_route_map.save('website/static/Destination_map.html')
-        return render_template('distance.html', landmarks=landmark,final_map='static/Destination_map.html',modes=['walk','bike','drive'],optims=['length','time'])
-    return render_template('distance.html',landmarks=landmark,final_map='static/Destination_map.html',modes=['walk','bike','drive'],optims=['length','time'])
+        return render_template('distance.html', landmarks=landmark,final_map='static/Destination_map.html',modes=['walk','bike','drive'],optims=['length','time'],Info=Info)
+    return render_template('distance.html',landmarks=landmark,final_map='static/Destination_map.html',modes=['walk','bike','drive'],optims=['length','time'],Info='Please select your source and target')
